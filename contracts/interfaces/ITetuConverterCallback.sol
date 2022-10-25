eb5271fc025040728e92254516b8df23b61173aa
dv <dvpublic0@gmail.com>
2022-10-25 17:39:47 +0700

    Add new functions to IKeeperCallback interface

--- contracts/interfaces/ITetuConverterCallback.sol
+++ contracts/interfaces/ITetuConverterCallback.sol
@@ -16,6 +16,18 @@ interface ITetuConverterCallback {
     uint amountToReturn_
   ) external returns (uint amountBorrowAssetReturned);
 
+  /// @notice TetuConverter calls this function when it needs to get additional collateral to fix health factor.
+  /// @param collateralAsset_ Collateral asset of the borrow to identify the borrow on the borrower's side
+  /// @param borrowAsset_ Borrow asset of the borrow to identify the borrow on the borrower's side
+  /// @param requiredCollateralAmount_  What amount of collateral asset the Borrower should send to TetuConverter
+  /// @return amountCollateralAssetOut Exact amount that borrower has sent to balance of TetuConverter
+  ///                                  It should be equal to requiredCollateralAmount_
+  function requireCollateralAmount(
+    address collateralAsset_,
+    address borrowAsset_,
+    uint requiredCollateralAmount_
+  ) external returns (uint amountCollateralAssetOut);
+
   /// @notice TetuConverter calls this function when it makes additional borrow (using exist collateral).
   ///         The given amount has already be sent to balance of the user, the user just should use it.
   /// @param collateralAsset_ Collateral asset of the borrow to identify the borrow on the borrower's side
@@ -25,5 +37,17 @@ interface ITetuConverterCallback {
     address collateralAsset_,
     address borrowAsset_,
     uint amountBorrowAssetSentToBorrower_
+  ) external;
+
+  /// @notice TetuConverter calls this function health factor becomes too healthy and it's possible to return
+  ///         some collateral amount back to the user.
+  ///         The given amount has already be sent to balance of the user, the user just should use it.
+  /// @param collateralAsset_ Collateral asset of the borrow to identify the borrow on the borrower's side
+  /// @param borrowAsset_ Borrow asset of the borrow to identify the borrow on the borrower's side
+  /// @param amountCollateralAssetSentToBorrower_ This amount has been sent to the borrower's balance
+  function onTransferCollateralAmount (
+    address collateralAsset_,
+    address borrowAsset_,
+    uint amountCollateralAssetSentToBorrower_
   ) external;
 }
