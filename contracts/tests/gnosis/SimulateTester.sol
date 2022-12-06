a4b5fb5faab519f3179aff63bf52fa8b50377d28
dv <dvpublic0@gmail.com>
2022-12-06 15:29:39 +0700

    test implementation of simulate to estimate target amount after swap

---
+++ contracts/tests/gnosis/SimulateTester.sol
@@ -0,0 +1,80 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.4;
+
+import "./SimulateContainer.sol";
+import "../../interfaces/ITetuLiquidator.sol";
+import "../../openzeppelin/IERC20.sol";
+import "../../openzeppelin/SafeERC20.sol";
+
+contract SimulateTester {
+  using SafeERC20 for IERC20;
+  uint public swapResult;
+
+  function makeSwapStub(uint inputValue) external returns (uint) {
+    swapResult = 77;
+    return swapResult + inputValue;
+  }
+
+  function callSimulateMakeSwapStub(SimulateContainer container) external returns (uint) {
+    console.log("callSimulateMakeSwapStub.1");
+    uint response = abi.decode(
+      container.simulate(
+        address(this),
+        abi.encodeWithSelector(SimulateTester.makeSwapStub.selector, 2)
+      ),
+      (uint)
+    );
+    console.log("callSimulateMakeSwapStub.response", response);
+    console.log("callSimulateMakeSwapStub.swapResult", swapResult);
+    return response;
+  }
+
+  function makeSwapUsingTetuLiquidator(
+    address tetuLiquidatorAddress,
+    address sourceToken_,
+    uint sourceAmount_,
+    address targetToken_
+  ) external returns (uint) {
+    console.log("makeSwapUsingTetuLiquidator", address(this), sourceAmount_);
+    uint balanceSourceBefore = IERC20(sourceToken_).balanceOf(address(this));
+    uint balanceTargetBefore = IERC20(targetToken_).balanceOf(address(this));
+    console.log("makeSwapUsingTetuLiquidator.balanceSourceBefore", balanceSourceBefore);
+    console.log("makeSwapUsingTetuLiquidator.balanceTargetBefore", balanceTargetBefore);
+
+    IERC20(sourceToken_).safeApprove(tetuLiquidatorAddress, sourceAmount_);
+    ITetuLiquidator tetuLiquidator = ITetuLiquidator(tetuLiquidatorAddress);
+    tetuLiquidator.liquidate(sourceToken_, targetToken_, sourceAmount_, 2000);
+    uint balanceSourceAfter = IERC20(sourceToken_).balanceOf(address(this));
+    uint balanceTargetAfter = IERC20(targetToken_).balanceOf(address(this));
+    console.log("source balance dif", balanceSourceBefore, balanceSourceAfter);
+    console.log("target balance dif", balanceTargetBefore, balanceTargetAfter);
+    return balanceTargetAfter - balanceTargetBefore;
+  }
+
+  function callSimulateMakeSwapUsingTetuLiquidator(
+    SimulateContainer container,
+    address tetuLiquidator,
+    address sourceAsset,
+    uint sourceAmount,
+    address targetAsset
+  ) external returns (uint) {
+    console.log("callSimulateSwap.1");
+
+    uint response = abi.decode(
+      container.simulate(
+        address(this),
+        abi.encodeWithSelector(
+            SimulateTester.makeSwapUsingTetuLiquidator.selector,
+            tetuLiquidator,
+            sourceAsset,
+            sourceAmount,
+            targetAsset
+        )
+      ),
+      (uint)
+    );
+    console.log("callSimulateSwap.response", response);
+    console.log("callSimulateSwap.swapResult", swapResult);
+    return response;
+  }
+}
---
+++ test/tests/SimulateTesterTest.ts
@@ -0,0 +1,139 @@
+import {DeployUtils} from "../../scripts/utils/DeployUtils";
+import {SignerWithAddress} from "@nomiclabs/hardhat-ethers/signers";
+import {TimeUtils} from "../../scripts/utils/TimeUtils";
+import {ethers} from "hardhat";
+import {IERC20__factory, IERC20Metadata__factory, SimulateContainer, SimulateTester} from "../../typechain";
+import {MaticAddresses} from "../../scripts/addresses/MaticAddresses";
+import {parseUnits} from "ethers/lib/utils";
+import {BalanceUtils} from "../baseUT/utils/BalanceUtils";
+import {ITetuLiquidator__factory} from "../../typechain/factories/contracts/interfaces";
+import {DeployerUtils} from "../../scripts/utils/DeployerUtils";
+
+describe("Test simulate tester", () => {
+  //region Global vars for all tests
+  let snapshot: string;
+  let snapshotForEach: string;
+  let deployer: SignerWithAddress;
+  let user1: SignerWithAddress;
+  let user2: SignerWithAddress;
+  let user3: SignerWithAddress;
+  let user4: SignerWithAddress;
+  let user5: SignerWithAddress;
+//endregion Global vars for all tests
+
+  //region before, after
+  before(async function () {
+    this.timeout(1200000);
+    snapshot = await TimeUtils.snapshot();
+    const signers = await ethers.getSigners();
+    deployer = signers[0];
+    user1 = signers[2];
+    user2 = signers[3];
+    user3 = signers[4];
+    user4 = signers[5];
+    user5 = signers[6];
+  });
+
+  after(async function () {
+    await TimeUtils.rollback(snapshot);
+  });
+
+  beforeEach(async function () {
+    snapshotForEach = await TimeUtils.snapshot();
+  });
+
+  afterEach(async function () {
+    await TimeUtils.rollback(snapshotForEach);
+  });
+//endregion before, after
+
+  it("stub-swap should return expected values", async () => {
+    const simulateContainer = await DeployUtils.deployContract(deployer, "SimulateContainer") as SimulateContainer;
+    const simulateTester = await DeployUtils.deployContract(deployer, "SimulateTester") as SimulateTester;
+
+    // const makeSwapCall = simulateTester.interface.encodeFunctionData("makeSwap", [2]);
+    await simulateTester.callSimulateMakeSwapStub(simulateContainer.address);
+  });
+
+  it("real-swap should return expected values", async () => {
+    const simulateContainer = await DeployUtils.deployContract(deployer, "SimulateContainer") as SimulateContainer;
+    const simulateTester = await DeployUtils.deployContract(deployer, "SimulateTester") as SimulateTester;
+
+    // const makeSwapCall = simulateTester.interface.encodeFunctionData("makeSwap", [2]);
+
+    await BalanceUtils.getRequiredAmountFromHolders(
+      parseUnits("100", 6),
+      IERC20Metadata__factory.connect(MaticAddresses.USDT, deployer),
+      [MaticAddresses.HOLDER_USDT_1],
+      simulateContainer.address
+    );
+    const usdt = IERC20__factory.connect(MaticAddresses.USDT, deployer);
+    const dai = IERC20__factory.connect(MaticAddresses.DAI, deployer);
+    console.log("simulateTester", simulateTester.address);
+    console.log("simulateContainer", simulateContainer.address);
+    console.log("usdt balance before", (await usdt.balanceOf(simulateContainer.address)).toString());
+    console.log("dai balance before", (await dai.balanceOf(simulateContainer.address)).toString());
+    await simulateTester.callSimulateMakeSwapUsingTetuLiquidator(
+      simulateContainer.address,
+      MaticAddresses.TETU_LIQUIDATOR,
+      MaticAddresses.USDT,
+      parseUnits("100", 6),
+      MaticAddresses.DAI
+    );
+    const ret = await simulateTester.callStatic.callSimulateMakeSwapUsingTetuLiquidator(
+      simulateContainer.address,
+      MaticAddresses.TETU_LIQUIDATOR,
+      MaticAddresses.USDT,
+      parseUnits("100", 6),
+      MaticAddresses.DAI
+    );
+    const gasUsed = await simulateTester.estimateGas.callSimulateMakeSwapUsingTetuLiquidator(
+      simulateContainer.address,
+      MaticAddresses.TETU_LIQUIDATOR,
+      MaticAddresses.USDT,
+      parseUnits("100", 6),
+      MaticAddresses.DAI
+    );
+    console.log("ret", ret);
+    console.log("gasUsed", gasUsed);
+    console.log("usdt balance after", (await usdt.balanceOf(simulateContainer.address)).toString());
+    console.log("dai balance after", (await dai.balanceOf(simulateContainer.address)).toString());
+  });
+
+  it("try to make real swap and check gas", async () => {
+    const swapper = ethers.Wallet.createRandom().address;
+    await BalanceUtils.getRequiredAmountFromHolders(
+      parseUnits("100", 6),
+      IERC20Metadata__factory.connect(MaticAddresses.USDT, deployer),
+      [MaticAddresses.HOLDER_USDT_1],
+      swapper
+    );
+    const usdtAsSwapper = IERC20__factory.connect(MaticAddresses.USDT, await DeployerUtils.startImpersonate(swapper));
+    const dai = IERC20__factory.connect(MaticAddresses.DAI, await DeployerUtils.startImpersonate(swapper));
+    console.log("swapper", swapper);
+    console.log("usdt balance before", (await usdtAsSwapper.balanceOf(swapper)).toString());
+    console.log("dai balance before", (await dai.balanceOf(swapper)).toString());
+    const tetuLiquidator = ITetuLiquidator__factory.connect(
+      MaticAddresses.TETU_LIQUIDATOR,
+      await DeployerUtils.startImpersonate(swapper)
+    );
+
+    await usdtAsSwapper.approve(tetuLiquidator.address, parseUnits("100", 6));
+    const gasUsed = await tetuLiquidator.estimateGas.liquidate(
+      MaticAddresses.USDT,
+      MaticAddresses.DAI,
+      parseUnits("100", 6),
+      2000
+    );
+    await tetuLiquidator.liquidate(
+      MaticAddresses.USDT,
+      MaticAddresses.DAI,
+      parseUnits("100", 6),
+      2000
+    );
+
+    console.log("gasUsed", gasUsed);
+    console.log("usdt balance after", (await usdtAsSwapper.balanceOf(swapper)).toString());
+    console.log("dai balance after", (await dai.balanceOf(swapper)).toString());
+  });
+});
