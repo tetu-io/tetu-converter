4dd73916ea9ae9e3d7c2592d0c22c7cfda960dfd
dv <dvpublic0@gmail.com>
2023-05-01 15:17:36 +0700

    remove hundred finance from coverage

--- .solcover.js
+++ .solcover.js
@@ -1,5 +1,5 @@
 module.exports = {
-  skipFiles: ['test', 'openzeppelin', 'lib', 'integration', 'interfaces'],
+  skipFiles: ['test', 'openzeppelin', 'lib', 'integration', 'interfaces', 'protocols/hundred-finance'],
   configureYulOptimizer: true,
   mocha: {
     grep: "@skip-on-coverage", // Find everything with this tag
