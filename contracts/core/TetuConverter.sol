7a21c2d2cd55dcab5482a13358866ff744590f3a
dv <dvpublic0@gmail.com>
2023-09-20 16:20:19 +0700

    fix coverage

--- contracts/core/TetuConverter.sol
+++ contracts/core/TetuConverter.sol
@@ -54,10 +54,12 @@ contract TetuConverter is ControllableV3, ITetuConverter, IKeeperCallback, IRequ
   /// @notice Local vars for {findConversionStrategy}
   struct FindConversionStrategyLocal {
     address[] borrowConverters;
+    address user;
+    address swapConverter;
+    IBorrowManager borrowManager;
     uint[] borrowSourceAmounts;
     uint[] borrowTargetAmounts;
     int[] borrowAprs18;
-    address swapConverter;
     uint swapSourceAmount;
     uint swapTargetAmount;
     int swapApr18;
@@ -117,21 +119,17 @@ contract TetuConverter is ControllableV3, ITetuConverter, IKeeperCallback, IRequ
 
     FindConversionStrategyLocal memory p;
     if (!_controller.paused()) {
+      // little gas optimization: skip any checking of exist debts if the user doesn't have any debts at all
+      p.borrowManager = IBorrowManager(_controller.borrowManager());
+      p.user = IDebtMonitor(_controller.debtMonitor()).getPositions(msg.sender, sourceToken_, targetToken_).length == 0
+        ? address(0)
+        : msg.sender;
+
       (p.borrowConverters,
         p.borrowSourceAmounts,
         p.borrowTargetAmounts,
         p.borrowAprs18
-      ) = IBorrowManager(_controller.borrowManager()).findConverter(
-        entryData_,
-      // little gas optimization: skip any checking of exist debts if the user doesn't have any debts at all
-        IDebtMonitor(_controller.debtMonitor()).getPositions(msg.sender, sourceToken_, targetToken_).length == 0
-          ? address(0)
-          : msg.sender,
-        sourceToken_,
-        targetToken_,
-        amountIn_,
-        periodInBlocks_
-      );
+      ) = p.borrowManager.findConverter(entryData_, p.user, sourceToken_, targetToken_, amountIn_, periodInBlocks_);
 
       (p.swapConverter,
         p.swapSourceAmount,
@@ -171,19 +169,17 @@ contract TetuConverter is ControllableV3, ITetuConverter, IKeeperCallback, IRequ
     require(periodInBlocks_ != 0, AppErrors.INCORRECT_VALUE);
 
     IConverterController _controller = IConverterController(controller());
-    return _controller.paused()
-      ? (converters, collateralAmountsOut, amountToBorrowsOut, aprs18) // no conversion is available
-      : IBorrowManager(_controller.borrowManager()).findConverter(
-        entryData_,
-        // little gas optimization: skip any checking of exist debts if the user doesn't have any debts at all
-        IDebtMonitor(_controller.debtMonitor()).getPositions(msg.sender, sourceToken_, targetToken_).length == 0
-          ? address(0)
-          : msg.sender,
-        sourceToken_,
-        targetToken_,
-        amountIn_,
-        periodInBlocks_
-      );
+    if (_controller.paused()) {
+      return (converters, collateralAmountsOut, amountToBorrowsOut, aprs18); // no conversion is available
+    } else {
+    // little gas optimization: skip any checking of exist debts if the user doesn't have any debts at all
+      address user = IDebtMonitor(_controller.debtMonitor()).getPositions(msg.sender, sourceToken_, targetToken_).length == 0
+        ? address(0)
+        : msg.sender;
+
+      IBorrowManager borrowManager = IBorrowManager(_controller.borrowManager());
+      return borrowManager.findConverter(entryData_, user, sourceToken_, targetToken_, amountIn_, periodInBlocks_);
+    }
   }
 
   /// @inheritdoc ITetuConverter
