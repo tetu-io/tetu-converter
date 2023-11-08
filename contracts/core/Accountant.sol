7ca16a1cec88c3083c30a56686c4c7008a312c5b
dv <dvpublic0@gmail.com>
2023-11-08 16:45:56 +0700

    scb-838: draft implementation of Accountant

--- contracts/core/Accountant.sol
+++ contracts/core/Accountant.sol
@@ -3,21 +3,27 @@ pragma solidity 0.8.17;
 
 import "../openzeppelin/IERC20.sol";
 import "../openzeppelin/SafeERC20.sol";
+import "../openzeppelin/EnumerableSet.sol";
+import "../openzeppelin/Math.sol";
 import "../interfaces/IAccountant.sol";
 import "../libs/AppUtils.sol";
 import "hardhat/console.sol";
+import "../proxy/ControllableV3.sol";
+import "../interfaces/IPoolAdapter.sol";
+import "../interfaces/IBorrowManager.sol";
 
 /// @notice Calculate amounts of losses and gains for debts/supply for all pool adapters
-contract Accountant is IAccountant {
+contract Accountant is IAccountant, ControllableV3 {
   using SafeERC20 for IERC20;
   using AppUtils for uint;
+  using EnumerableSet for EnumerableSet.AddressSet;
 
   //region ----------------------------------------------------- Constants
   string public constant ACCOUNTANT_VERSION = "1.0.0";
   //endregion ----------------------------------------------------- Constants
 
   //region ----------------------------------------------------- Data types
-  struct UserState {
+  struct PoolAdapterState {
     /// @notice Current total amount supplied by the user as a collateral
     uint suppliedAmount;
     /// @notice Current total borrowed amount
@@ -28,114 +34,258 @@ contract Accountant is IAccountant {
     /// @notice Current total debt registered on the lending platform
     uint lastTotalDebt;
   }
+
+  struct GainLossInfo {
+    /// @notice Gain (received for supplied amount) registered in the current period, in terms of underlying
+    int gain;
+    /// @notice Losses (paid for the borrowed amount) registered in the current period, in terms of underlying
+    int losses;
+  }
+
+  struct OnRepayLocal {
+    IConverterController controller;
+    IBorrowManager borrowManager;
+    IPoolAdapter poolAdapter;
+    address user;
+    uint totalCollateral;
+    uint totalDebt;
+    uint debtRatio;
+    uint collateralRatio;
+    uint debt;
+    uint collateral;
+  }
   //endregion ----------------------------------------------------- Data types
 
   //region ----------------------------------------------------- Variables
+  /// @notice Counter of the periods
+  uint public period;
 
-  /// @notice user (pool adapter) => state
-  mapping(address => UserState) internal _userStates;
+  /// @notice pool adapter => current state
+  mapping(address => PoolAdapterState) internal _states;
 
+  /// @notice pool adapter => gains and losses info
+  mapping(address => GainLossInfo) internal _losses;
+
+  /// @notice User of the pool adapter => list of pool adapters with not zero debts in the current period
+  mapping(address => EnumerableSet.AddressSet) private _poolAdaptersPerUser;
   //endregion ----------------------------------------------------- Variables
 
-  //region ----------------------------------------------------- IAccountant
+  //region ----------------------------------------------------- Events
+  event OnBorrow(address poolAdapter, uint collateralAmount, uint borrowedAmount);
+
+  /// @param gain Gain in terms of collateral
+  /// @param losses Losses in terms of borrow asset
+  /// @param gainInUnderlying Gain in terms of underlying
+  /// @param lossesInUnderlying Losses in terms of underlying
+  event OnRepay(
+    address poolAdapter,
+    uint withdrawnCollateral,
+    uint paidAmount,
+    uint gain,
+    uint losses,
+    uint gainInUnderlying,
+    uint lossesInUnderlying
+  );
+  //endregion ----------------------------------------------------- Events
+
+  //region ----------------------------------------------------- Initialization
+  function init(address controller_) external initializer {
+    __Controllable_init(controller_);
+  }
+  //endregion ----------------------------------------------------- Initialization
+
+  //region ----------------------------------------------------- OnBorrow, OnRepay
   /// @notice Register a new loan
   /// @param collateralAmount Amount of supplied collateral for the new loan
   /// @param borrowedAmount Borrowed amount provided for the given {collateralAmount}
-  /// @param totalCollateral Total amount of collateral supplied by the user, at the moment after the borrowing.
-  /// @param totalDebt Total debt of the user, at the moment after the borrowing.
-  function onBorrow(
-    uint collateralAmount,
-    uint borrowedAmount,
-    uint totalCollateral,
-    uint totalDebt
-  ) external {
-    // todo Pool adapter only
+  function onBorrow(uint collateralAmount, uint borrowedAmount) external {
+    IConverterController _controller = IConverterController(controller());
+    IBorrowManager borrowManager = IBorrowManager(_controller.borrowManager());
+    require(borrowManager.isPoolAdapter(msg.sender), AppErrors.POOL_ADAPTER_NOT_FOUND);
+
+    IPoolAdapter pa = IPoolAdapter(msg.sender);
+    (, address user, , ) = pa.getConfig();
+
+    (uint totalCollateral, uint totalDebt,,,,) = pa.getStatus();
+
     console.log("onBorrow");
 
-    UserState memory state = _userStates[msg.sender];
+    PoolAdapterState memory state = _states[msg.sender];
     console.log("onBorrow.state.suppliedAmount", state.suppliedAmount);
     console.log("onBorrow.state.borrowedAmount", state.borrowedAmount);
     console.log("onBorrow.state.lastTotalCollateral", state.lastTotalCollateral);
     console.log("onBorrow.state.lastTotalDebt", state.lastTotalDebt);
 
-    state = UserState({
+    _states[msg.sender] = PoolAdapterState({
       suppliedAmount: state.suppliedAmount + collateralAmount,
       borrowedAmount: state.borrowedAmount + borrowedAmount,
       lastTotalCollateral: totalCollateral,
       lastTotalDebt: totalDebt
     });
-    _userStates[msg.sender] = state;
 
-    console.log("onBorrow.2.state.suppliedAmount", state.suppliedAmount);
-    console.log("onBorrow.2.state.borrowedAmount", state.borrowedAmount);
-    console.log("onBorrow.2.state.lastTotalCollateral", state.lastTotalCollateral);
-    console.log("onBorrow.2.state.lastTotalDebt", state.lastTotalDebt);
+    console.log("onBorrow.2.state.suppliedAmount", _states[msg.sender].suppliedAmount);
+    console.log("onBorrow.2.state.borrowedAmount", _states[msg.sender].borrowedAmount);
+    console.log("onBorrow.2.state.lastTotalCollateral", _states[msg.sender].lastTotalCollateral);
+    console.log("onBorrow.2.state.lastTotalDebt", _states[msg.sender].lastTotalDebt);
+
+    _poolAdaptersPerUser[user].add(msg.sender);
   }
 
   /// @notice Register loan payment
   /// @param withdrawnCollateral Amount of collateral received by the user during the repaying.
   /// @param paidAmount Amount paid by the user during the repaying.
-  /// @param totalCollateral Total amount of collateral supplied by the user, at the moment after the repaying.
-  /// @param totalDebt Total debt of the user, at the moment after the repaying.
-  /// @return gain Amount of collateral earned by the loan in terms of collateral. Positive means profit.
-  /// @return losses Loan repayment losses in terms of borrowed amount. Positive means losses.
-  function onRepay(
-    uint withdrawnCollateral,
-    uint paidAmount,
-    uint totalCollateral,
-    uint totalDebt
-  ) external returns (
-    int gain,
-    int losses
-  ) {
-    // todo Pool adapter only
-    UserState memory state = _userStates[msg.sender];
-    // todo require debt is not zero ???
-    console.log("onRepay.1.state.suppliedAmount", state.suppliedAmount);
-    console.log("onRepay.1.state.borrowedAmount", state.borrowedAmount);
-    console.log("onRepay.1.state.lastTotalCollateral", state.lastTotalCollateral);
-    console.log("onRepay.1.state.lastTotalDebt", state.lastTotalDebt);
+  function onRepay(uint withdrawnCollateral, uint paidAmount) external {
+    OnRepayLocal memory v;
 
-    uint debtRatio = 1e18 * paidAmount / (totalDebt + paidAmount);
-    uint collateralRatio = 1e18 * withdrawnCollateral / (totalCollateral + withdrawnCollateral);
+    v.controller = IConverterController(controller());
+    v.borrowManager = IBorrowManager(v.controller.borrowManager());
 
-    uint debt = state.borrowedAmount * debtRatio / 1e18;
-    uint collateral = state.suppliedAmount * collateralRatio / 1e18;
+    // onRepay is allowed for registered platform adapters only
+    // if some dirty (de-registered) pool adapter will try to call onRepay
+    // we shouldn't prevent its attempt to make repay but
+    // it's too dangerous to take results into account here because a malicious contract can try to give us wrong data
+    // so, no revert, silent ignore
 
-    console.log("onRepay.debtRatio", debtRatio);
-    console.log("onRepay.collateralRatio", collateralRatio);
-    console.log("onRepay.debt", debt);
-    console.log("onRepay.collateral", collateral);
+    if (v.borrowManager.isPoolAdapter(msg.sender)) {
+      v.poolAdapter = IPoolAdapter(msg.sender);
+      (, v.user, , ) = v.poolAdapter.getConfig();
+      (v.totalCollateral, v.totalDebt,,,,) = v.poolAdapter.getStatus();
 
-    state = UserState({
-      borrowedAmount: state.borrowedAmount - debt,
-      suppliedAmount: state.suppliedAmount - collateral,
-      lastTotalCollateral: totalCollateral,
-      lastTotalDebt: totalDebt
-    });
+      PoolAdapterState memory state = _states[msg.sender];
+      // todo require debt is not zero ???
+      console.log("onRepay.1.state.suppliedAmount", state.suppliedAmount);
+      console.log("onRepay.1.state.borrowedAmount", state.borrowedAmount);
+      console.log("onRepay.1.state.lastTotalCollateral", state.lastTotalCollateral);
+      console.log("onRepay.1.state.lastTotalDebt", state.lastTotalDebt);
 
-    gain = int(withdrawnCollateral) - int(collateral);
-    losses = int(paidAmount) - int(debt);
-    console.log("onRepay.gain");console.logInt(gain);
-    console.log("onRepay.losses");console.logInt(losses);
+      v.debtRatio = Math.min(1e18, 1e18 * paidAmount / (v.totalDebt + paidAmount));
+      v.collateralRatio = Math.min(1e18, 1e18 * withdrawnCollateral / (v.totalCollateral + withdrawnCollateral));
 
-    _userStates[msg.sender] = state;
-    console.log("onRepay.2.state.suppliedAmount", state.suppliedAmount);
-    console.log("onRepay.2.state.borrowedAmount", state.borrowedAmount);
-    console.log("onRepay.2.state.lastTotalCollateral", state.lastTotalCollateral);
-    console.log("onRepay.2.state.lastTotalDebt", state.lastTotalDebt);
+      v.debt = state.borrowedAmount * v.debtRatio / 1e18;
+      v.collateral = state.suppliedAmount * v.collateralRatio / 1e18;
+
+      console.log("onRepay.debtRatio", v.debtRatio);
+      console.log("onRepay.collateralRatio", v.collateralRatio);
+      console.log("onRepay.debt", v.debt);
+      console.log("onRepay.collateral", v.collateral);
+
+      _states[msg.sender] = PoolAdapterState({
+        borrowedAmount: state.borrowedAmount - v.debt,
+        suppliedAmount: state.suppliedAmount - v.collateral,
+        lastTotalCollateral: v.totalCollateral,
+        lastTotalDebt: v.totalDebt
+      });
+
+      int gain = int(withdrawnCollateral) - int(v.collateral);
+      int losses = int(paidAmount) - int(v.debt);
+
+      int gainInUnderlying = gain; // todo
+      int lossesInUnderlying = losses; // todo
+
+      GainLossInfo memory prev = _losses[v.user];
+      _losses[v.user] = GainLossInfo({
+        losses: prev.losses + losses,
+        gain: prev.gain + gain
+      });
+
+      console.log("onRepay.gain");console.logInt(gain);
+      console.log("onRepay.losses");console.logInt(losses);
+
+      console.log("onRepay.2.state.suppliedAmount", _states[msg.sender].suppliedAmount);
+      console.log("onRepay.2.state.borrowedAmount", _states[msg.sender].borrowedAmount);
+      console.log("onRepay.2.state.lastTotalCollateral", _states[msg.sender].lastTotalCollateral);
+      console.log("onRepay.2.state.lastTotalDebt", _states[msg.sender].lastTotalDebt);
+
+      _poolAdaptersPerUser[v.user].add(msg.sender);
+    }
   }
-  //endregion ----------------------------------------------------- IAccountant
+  //endregion ----------------------------------------------------- OnBorrow, OnRepay
 
-  //region ----------------------------------------------------- View
-  function getUserState(address user) external view returns (
+  //region ----------------------------------------------------- Logic for period
+  /// @notice Start new period of collecting of gains and losses.
+  ///         Set current total gains and total losses for each pool adapter to zero.
+  ///         Remove pool adapters with zero debts from the user.
+  /// @return totalGain Total amount of collateral earned by the pool adapters in the previous period,
+  ///                   in terms of underlying
+  /// @return totalLosses Total loan repayment losses in terms of borrowed amount in the previous period,
+  ///                     in terms of underlying
+  function startNewPeriod(address user) external returns (int totalGain, int totalLosses) {
+    period += 1;
+
+    uint countPoolAdapters = _poolAdaptersPerUser[user].length();
+    for (uint i = countPoolAdapters; i > 0; i--) {
+      address poolAdapter = _poolAdaptersPerUser[user].at(i - 1);
+      GainLossInfo memory lossInfo = _losses[poolAdapter];
+      totalGain += lossInfo.gain;
+      totalLosses += lossInfo.losses;
+      (,,, bool opened, ,) = IPoolAdapter(poolAdapter).getStatus();
+      if (! opened) {
+        delete _losses[poolAdapter];
+        delete _states[poolAdapter];
+        _poolAdaptersPerUser[user].remove(poolAdapter);
+      }
+    }
+    return (totalGain, totalLosses);
+  }
+
+  /// @notice Get current state of the given pool adapter
+  /// @return totalGain Total amount of collateral earned by the loan in the current period, in terms of underlying.
+  ///                   Positive means profit.
+  /// @return totalLosses Total loan repayment losses in the current period, in terms of underlying.
+  ///                     Positive means losses.
+  /// @return suppliedAmount Current total amount supplied by the user as a collateral
+  /// @return borrowedAmount Current total borrowed amount
+  /// @return lastTotalCollateral Current total amount of collateral registered on the lending platform
+  /// @return lastTotalDebt Current total debt registered on the lending platform
+  function getPoolAdapterState(address poolAdapter) external view returns (
+    int totalGain,
+    int totalLosses,
     uint suppliedAmount,
     uint borrowedAmount,
     uint lastTotalCollateral,
     uint lastTotalDebt
   ) {
-    UserState memory state = _userStates[user];
-    return (state.suppliedAmount, state.borrowedAmount, state.lastTotalCollateral, state.lastTotalDebt);
+    PoolAdapterState memory state = _states[poolAdapter];
+    GainLossInfo memory lossInfo = _losses[poolAdapter];
+
+    return (
+      lossInfo.gain,
+      lossInfo.losses,
+      state.suppliedAmount,
+      state.borrowedAmount,
+      state.lastTotalCollateral,
+      state.lastTotalDebt
+    );
+  }
+
+  /// @notice Get current state of the given user (== strategy, the user of pool adapters)
+  /// @return countPoolAdapters Current count of pool adapters
+  /// @return totalGain Total amount of collateral earned by the pool adapters in the current period,
+  ///                   in terms of underlying
+  /// @return totalLosses Total loan repayment losses in terms of borrowed amount in the current period,
+  ///                     in terms of underlying
+  function getStateForPeriod(address user) external view returns (
+    uint countPoolAdapters,
+    int totalGain,
+    int totalLosses
+  ) {
+    countPoolAdapters = _poolAdaptersPerUser[user].length();
+    for (uint i; i < countPoolAdapters; ++i) {
+      address poolAdapter = _poolAdaptersPerUser[user].at(i);
+      GainLossInfo memory lossInfo = _losses[poolAdapter];
+      totalGain += lossInfo.gain;
+      totalLosses += lossInfo.losses;
+    }
+    return (countPoolAdapters, totalGain, totalLosses);
+  }
+  //endregion ----------------------------------------------------- Logic for period
+
+  //region ----------------------------------------------------- Utils
+  function poolAdaptersPerUserLength(address user) external view returns (uint) {
+    return _poolAdaptersPerUser[user].length();
+  }
+  function poolAdaptersPerUserAt(address user, uint index) external view returns (address) {
+    return _poolAdaptersPerUser[user].at(index);
   }
   //endregion ----------------------------------------------------- View
 
--- contracts/interfaces/IAccountant.sol
+++ contracts/interfaces/IAccountant.sol
@@ -3,31 +3,53 @@ pragma solidity 0.8.17;
 
 interface IAccountant {
   /// @notice Register a new loan
+  /// @dev This function can be called by a pool adapter only
   /// @param collateralAmount Amount of supplied collateral for the new loan
   /// @param borrowedAmount Borrowed amount provided for the given {collateralAmount}
-  /// @param totalCollateral Total amount of collateral supplied by the user, at the moment after the borrowing.
-  /// @param totalDebt Total debt of the user, at the moment after the borrowing.
-  function onBorrow(
-    uint collateralAmount,
-    uint borrowedAmount,
-    uint totalCollateral,
-    uint totalDebt
-  ) external;
+  function onBorrow(uint collateralAmount, uint borrowedAmount) external;
 
   /// @notice Register loan payment
+  /// @dev This function can be called by a pool adapter only
   /// @param withdrawnCollateral Amount of collateral received by the user during the repaying.
   /// @param paidAmount Amount paid by the user during the repaying.
-  /// @param totalCollateral Total amount of collateral supplied by the user, at the moment after the repaying.
-  /// @param totalDebt Total debt of the user, at the moment after the repaying.
-  /// @return gain Amount of collateral earned by the loan in terms of collateral. Positive means profit.
-  /// @return losses Loan repayment losses in terms of borrowed amount. Positive means losses.
-  function onRepay(
-    uint withdrawnCollateral,
-    uint paidAmount,
-    uint totalCollateral,
-    uint totalDebt
-  ) external returns (
-    int gain,
-    int losses
+  function onRepay(uint withdrawnCollateral, uint paidAmount) external;
+
+  /// @notice Get current state of the given pool adapter
+  /// @return totalGain Total amount of collateral earned by the loan in the current period, in terms of underlying.
+  ///                   Positive means profit.
+  /// @return totalLosses Total loan repayment losses in the current period, in terms of underlying.
+  ///                     Positive means losses.
+  /// @return suppliedAmount Current total amount supplied by the user as a collateral
+  /// @return borrowedAmount Current total borrowed amount
+  /// @return lastTotalCollateral Current total amount of collateral registered on the lending platform
+  /// @return lastTotalDebt Current total debt registered on the lending platform
+  function getPoolAdapterState(address poolAdapter) external view returns (
+    int totalGain,
+    int totalLosses,
+    uint suppliedAmount,
+    uint borrowedAmount,
+    uint lastTotalCollateral,
+    uint lastTotalDebt
   );
+
+  /// @notice Get current state of the given user (== strategy, the user of pool adapters)
+  /// @return countPoolAdapters Current count of pool adapters
+  /// @return totalGain Total amount of collateral earned by the pool adapters in the current period,
+  ///                   in terms of underlying
+  /// @return totalLosses Total loan repayment losses in terms of borrowed amount in the current period,
+  ///                     in terms of underlying
+  function getStateForPeriod(address user) external view returns (
+    uint countPoolAdapters,
+    int totalGain,
+    int totalLosses
+  );
+
+  /// @notice Start new period of collecting of gains and losses.
+  ///         Set current total gains and total losses for each pool adapter to zero.
+  ///         Remove pool adapters with zero debts from the user.
+  /// @return totalGain Total amount of collateral earned by the pool adapters in the previous period,
+  ///                   in terms of underlying
+  /// @return totalLosses Total loan repayment losses in terms of borrowed amount in the previous period,
+  ///                     in terms of underlying
+  function startNewPeriod(address user) external returns (int totalGain, int totalLosses);
 }
